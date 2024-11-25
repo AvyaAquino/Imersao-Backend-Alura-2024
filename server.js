670e99e85c200d201b75b8c8b16ec61a1e20a0be
avyniee <avyagard@gmail.com>
2024-11-25 16:19:34 -0300

    Imersão Backend Alura: Aula 2

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 import express from "express";
 
+const posts = [
+    {
+      descricao: "fototeste",
+      imagem: "https://placecats.com/millie/300/150",
+      id: 1
+    },
+    {
+      descricao: "Gato brincando com bola",
+      imagem: "https://placecats.com/millie/300/150",
+      id: 2
+    },
+    {
+      descricao: "Gatinho fazendo panqueca",
+      imagem: "https://placecats.com/millie/300/150",
+      id: 3
+    }
+  ];
+
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor Escutando");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas Vindas à imersão");
+app.get("/posts/", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => post.id === Number(id));
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
